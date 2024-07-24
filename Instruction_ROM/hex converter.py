05897189e3a79db97cf105d93b9b79ab2e820fab
Isaac Melo <isaac.melo426@gmail.com>
2024-07-24 14:52:49 -0400

    "changed the ROM to hex"

--- Instruction_ROM/hex converter.py
+++ Instruction_ROM/hex converter.py
@@ -1,6 +1,8 @@
 def convert_to_hex():
-    with open("decodeROM.txt","r") as file:
+    with open("decodeROM.txt","r+") as file:
         for i in range(15):
             for j in range(8):
-                element = file.readline()
-                
+                element = bytes(file.readline())
+                element = hex(element)
+convert_to_hex()
+
--- Instruction_ROM/what.c
+++ Instruction_ROM/what.c
@@ -23,20 +23,11 @@
 #define S1 0b000000000000000000010
 #define S2 0b000000000000000000001
 
-// Function to convert an integer to a binary string
-void printBinary(int number, char* binaryString) {
-    for (int i = 20; i >= 0; i--) {
-        binaryString[20 - i] = ((number >> i) & 1) + '0';  // Fixed indexing to match 21 bits
-    }
-    binaryString[21] = '\0';  // Null-terminate the string
+
+void hex(char* binarystring, int number){
+    sprintf(binarystring,"%X", number);
 }
 
-void sixbithex(int number, char* binaryString){
-    for (int i = 5; i >= 0; i--) {
-        binaryString[5 - i] =((number >> i) & 1) + '0';  // Fixed indexing to match 21 bits
-    }
-    binaryString[6] = '\0';  // Null-terminate the string
-}
 
 int main() {
     FILE *pf = fopen("decodeROM.txt", "w");
@@ -66,17 +57,19 @@ int main() {
     
     int size = sizeof(data) / sizeof(data[0]);
     int counter = 0;
+    
     for (int i = 0; i < size; i++) {
         if(counter == 8){
             fprintf(pf,"\n");
             counter = 0;
         }
         char binaryString[22];  
-        printBinary(data[i], binaryString);  
-        
+        hex(binaryString,data[i]);
+
         fprintf(pf, "%s%s", binaryString," ");  
         counter ++;
     }
+    
 
     fclose(pf);
 
